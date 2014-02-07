52a51b8e3bc9b2d09cc67bbe1466a86d09d601c6
sami3000 <sam.ronsin@gmail.com>
2014-02-07 19:14:10 +0100

    correction 'webpage'->'project_page'

--- project_scrapping.py
+++ project_scrapping.py
@@ -24,16 +24,16 @@ def project_title(project_page):
     return re.search(u'(class=\"NS-project_-running_board\">\n<h2 id=\"title\">\n<a href=\"\/projects\/)(.*)(\">)(.*)(<)' , webpage).groups()[3]
 
 def nb_backers(project_page):
-    return int(re.search(u'(\"Project\[backers_count\]\">)([0-9]+)' , webpage).groups()[1])
+    return int(re.search(u'(\"Project\[backers_count\]\">)([0-9]+)', project_page).groups()[1])
 
 def project_status(project_page):
     return re.search(u'(Project-state-)(.*)( Project-is)', project_page).groups()[1]
 
 def project_goal(project_page):
-    return float(re.search(u'(data-goal=\")([0-9]+\.[0-9]+)', webpage).groups()[1])
+    return float(re.search(u'(data-goal=\")([0-9]+\.[0-9]+)', project_page).groups()[1])
 
 def nb_rewards(project_page):
-    return int(re.search(u'(data-reward-count=\")([0-9]+)',webpage).groups()[1])
+    return int(re.search(u'(data-reward-count=\")([0-9]+)', project_page).groups()[1])
 
 def parse_raw_int(s):
     new_s = s.replace(",", "")
@@ -59,7 +59,6 @@ def nb_backers_list(project_page):
 
 
 def print_info(project_page):
-
     print "title: \'"+project_title(project_page)+"\'"
     print "goal: "+str(project_goal(project_page))
     print str(nb_rewards(project_page))+" rewards"
