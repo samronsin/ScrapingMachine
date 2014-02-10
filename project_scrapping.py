bb081c71e94d0d5e4e075373187fbb85bfbebf98
Subacultchah <subacultchah@googlemail.com>
2014-02-10 18:09:44 +0100

    correction des bugs de merge

--- project_scrapping.py
+++ project_scrapping.py
@@ -16,7 +16,6 @@ import urllib
 
 
 webpage = urllib.urlopen('http://www.kickstarter.com/projects/lalorek/a-field-guide-to-silicon-hills').read()
-#print webpage
 
 project_head = re.search(u'(class=\"NS-project_-running_board\">\n<h2 id=\"title\">\n<a href=\"\/projects\/)(.*)(\">)(.*)(<)' , webpage)
 href = project_head.groups()[1]
@@ -27,31 +26,25 @@ def project_title(project_page):
     return re.search(u'(class=\"NS-project_-running_board\">\n<h2 id=\"title\">\n<a href=\"\/projects\/)(.*)(\">)(.*)(<)' , project_page).groups()[3]
 
 def nb_backers(project_page):
-<<<<<<< HEAD
     return int(re.search(u'(\"Project\[backers_count\]\">)([0-9]+)' , project_page).groups()[1])
-=======
-    return int(re.search(u'(\"Project\[backers_count\]\">)([0-9]+)', project_page).groups()[1])
->>>>>>> b7597221a0fec58a0408873986ae08f11ab9723a
+ 
 
 def project_status(project_page):
     return re.search(u'(Project-state-)(.*)( Project-is)', project_page).groups()[1]
 
 def project_goal(project_page):
     return float(re.search(u'(data-goal=\")([0-9]+\.[0-9]+)', project_page).groups()[1])
-<<<<<<< HEAD
-
+ 
 
 def project_money(project_page):
     return float(re.search(u'(data-pledged=\")([0-9]+\.[0-9]+)', project_page).groups()[1])
 
 def nb_rewards(project_page):
     return int(re.search(u'(data-reward-count=\")([0-9]+)',project_page).groups()[1])
-=======
-
+ 
 def nb_rewards(project_page):
     return int(re.search(u'(data-reward-count=\")([0-9]+)', project_page).groups()[1])
->>>>>>> b7597221a0fec58a0408873986ae08f11ab9723a
-
+ 
 def parse_raw_int(s):
     new_s = s.replace(",", "")
     return int(new_s)
@@ -75,8 +68,7 @@ def nb_backers_list(project_page):
 def clean(var): return var.upper().replace('_',' ')
 
 
-<<<<<<< HEAD
-current_vars=["project_title" ,"nb_backers" ,"project_status" ,"project_goal" ,"project_money" ,"nb_rewards" ,"parse_raw_int" ,"rewards_list" ,"nb_backers_list"]
+ current_vars=["project_title" ,"nb_backers" ,"project_status" ,"project_goal" ,"project_money" ,"nb_rewards", "rewards_list" ,"nb_backers_list"]
 def print_info(project_page, list_var):
     for var in list_var:
         print "%s : \t %s" %(clean(var), globals()[var](project_page)) #globals() est un dictionnaire de toutes les fonctions créés dans la session, il associe le nom de la fonction à la fonction
@@ -86,15 +78,6 @@ def get_data(project_page, list_var)
     for var in list_var:
         data[clean(var)] = globals()[var](project_page)
     return data
-=======
-def print_info(project_page):
-    print "title: \'"+project_title(project_page)+"\'"
-    print "goal: "+str(project_goal(project_page))
-    print str(nb_rewards(project_page))+" rewards"
-    print rewards_list(project_page)
-    print str(nb_backers(project_page))+" backers"
-    print nb_backers_list(project_page)
->>>>>>> b7597221a0fec58a0408873986ae08f11ab9723a
 
     
 print_info(webpage, current_vars)
