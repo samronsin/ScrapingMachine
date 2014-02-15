76ebe03d0b7c1987df5d4208dfe5edd725db6a14
Subacultchah <subacultchah@googlemail.com>
2014-02-15 14:26:20 +0100

    1) creation du fichier avec la liste des projets 2) création du fichier starter qui va lire la liste des projets et telecharge le contenu

--- KS_scrapping.py
+++ KS_scrapping.py
@@ -7,6 +7,9 @@ import re
 
 
 def projects_list():
+    """Récupère la liste brute des nouveaux projets
+    ne poursuis pas au delà des 20 premiers projets 
+    (cf bouton 'load more' qu'on n'active pas)"""
     webpage = urllib.urlopen('http://www.kickstarter.com/discover/advanced?sort=launch_date').read()
     projects_list_raw = re.findall(u'(/projects/)(.*)(\?ref=)' , webpage)
     n_projects = len(projects_list_raw)/2
@@ -15,4 +18,24 @@ def projects_list():
         projects[i] = projects_list_raw[2*i][1]
     return projects
 
-projects_list()
+#print projects_list()
+#on crée la liste des projets, ( identifiant, short_url)
+
+with open('list_projects.txt', 'w+') as list_projects_file :  #la syntaxe 'with ... as :' permet d'éviter d'oublier un file.close, le fichier est automatique fermé en sortie de boucle    
+    if len(list_projects_file.read()) == 0 :
+        data = [[]]
+        list_urls = []
+        next_id = 0
+    else :	
+        data      =  [ project.strip().split(';') for project in list_projects_file.readlines() ]
+        list_urls =   [ project[1] for project  in data ] # data[:,1] ne marche pas c'est génial
+        list_id   =  [float(project[0]) for project  in data ]
+        if len(list_id) > 0 :
+            next_id = max(list_id ) + 1
+        else :
+            next_id = 1    
+
+    for new_project_url in projects_list() :
+        if new_project_url not in list_urls :
+            list_projects_file.write( str(next_id) + ';' + new_project_url +  '\n') 
+            next_id += 1    
--- project_scrapping.py
+++ project_scrapping.py
@@ -7,20 +7,36 @@ import re
 import unicodedata
 import json
 import urllib
+import datetime
 #TODO:
 #  - replace regex calls by XPath
 #  - one single parsing of the project page to get all the info
 #  - replace the calls to print (on the screen) by writing on a DB
 #  - refléchir à l'optimisation de l'ouverture de page (L17), qui prend plus d'une seconde
-#  - ajouter d'autres 
+#  - ajouter d'autres  
+#  - récupérer le type de l'erreur au lieu de relever simplement 'erreur' L100
 
 
-webpage = urllib.urlopen('http://www.kickstarter.com/projects/lalorek/a-field-guide-to-silicon-hills').read()
+#webpage = urllib.urlopen('http://www.kickstarter.com/projects/lalorek/a-field-guide-to-silicon-hills').read()
 
-project_head = re.search(u'(class=\"NS-project_-running_board\">\n<h2 id=\"title\">\n<a href=\"\/projects\/)(.*)(\">)(.*)(<)' , webpage)
-href = project_head.groups()[1]
-project_title = project_head.groups()[3]
 
+#### Fonctions de nettoyage de données
+#########################################################
+
+
+def parse_raw_int(s):
+    new_s = s.replace(",", "")
+    return int(new_s)
+
+
+def clean_varnames(var): return var.upper().replace('_',' ')
+
+
+#### Fonctions pour chercher les informations sur la page
+#########################################################
+
+def creator_name(project_page):
+ return re.search(u'data-modal-title=\"Biography\" id=\"name\">(.*)<', project_page).groups()[0]
 
 def project_title(project_page):
     return re.search(u'(class=\"NS-project_-running_board\">\n<h2 id=\"title\">\n<a href=\"\/projects\/)(.*)(\">)(.*)(<)' , project_page).groups()[3]
@@ -28,9 +44,9 @@ def project_title(project_page):
 def nb_backers(project_page):
     return int(re.search(u'(\"Project\[backers_count\]\">)([0-9]+)' , project_page).groups()[1])
  
-
-def project_status(project_page):
-    return re.search(u'(Project-state-)(.*)( Project-is)', project_page).groups()[1]
+###bon ca se buggue va savoir, je le retire aussi de la liste L101
+#def project_status(project_page):
+#    return re.search(u'(Project-state-)(.*)( Project-is)', project_page).groups()[1] 
 
 def project_goal(project_page):
     return float(re.search(u'(data-goal=\")([0-9]+\.[0-9]+)', project_page).groups()[1])
@@ -45,9 +61,6 @@ def nb_rewards(project_page):
 def nb_rewards(project_page):
     return int(re.search(u'(data-reward-count=\")([0-9]+)', project_page).groups()[1])
  
-def parse_raw_int(s):
-    new_s = s.replace(",", "")
-    return int(new_s)
 
 def rewards_list(project_page):
     rewards_list_raw = re.findall(u'(<h5>\nPledge\n<span class=\"money usd \">\$)([0-9]*.*[0-9]+)', project_page)
@@ -65,19 +78,32 @@ def nb_backers_list(project_page):
         nb_backers_list[i] = parse_raw_int(nb_backers_list_raw[i][1])
     return nb_backers_list
 
-def clean(var): return var.upper().replace('_',' ')
 
 
- current_vars=["project_title" ,"nb_backers" ,"project_status" ,"project_goal" ,"project_money" ,"nb_rewards", "rewards_list" ,"nb_backers_list"]
+#### Fonctions d'output
+#########################################################
+
+def scrapping_day(project_page): return str(datetime.datetime.now().date())
+def scrapping_time(project_page): return str(datetime.datetime.now())
+
+
 def print_info(project_page, list_var):
     for var in list_var:
-        print "%s : \t %s" %(clean(var), globals()[var](project_page)) #globals() est un dictionnaire de toutes les fonctions créés dans la session, il associe le nom de la fonction à la fonction
+        print "%s : \t %s" %(clean_varnames(var), globals()[var](project_page)) #globals() est un dictionnaire de toutes les fonctions créés dans la session, il associe le nom de la fonction à la fonction
 
-def get_data(project_page, list_var)
+def get_data(project_page, list_var):
     data={}                                 #on crée un dictionnaire qui associera à chaque var de list_var, sa valeur dans projectpage
     for var in list_var:
-        data[clean(var)] = globals()[var](project_page)
+        try :
+            data[clean_varnames(var)] = globals()[var](project_page)
+        except :
+            data[clean_varnames(var)] = 'error' #ici changer pour obtenir le type de l'erreur
+
     return data
 
+
+
+current_vars=["scrapping_day", "scrapping_time", "project_title" ,"nb_backers" ,"project_goal" ,"project_money" ,"nb_rewards", "rewards_list" ,"nb_backers_list", "creator_name"]
+
     
-print_info(webpage, current_vars)
+#print_info(webpage, current_vars)
