a869a856fe089f6606387f110f10f08ce9d58c2e
Subacultchah <subacultchah@googlemail.com>
2014-02-16 18:56:33 +0100

    correction écrasement de liste projets

--- KS_scrapping.py
+++ KS_scrapping.py
@@ -25,17 +25,18 @@ def projects_list():
 #print projects_list()
 #on crée la liste des projets, ( identifiant, short_url), en vérifiant bien que la short url n'est pas dans la liste
 
-with open('list_projects.txt', 'a+') as list_projects_file :  #la syntaxe 'with ... as :' permet d'éviter d'oublier un file.close, le fichier est automatique fermé en sortie de boucle    
+with open('list_projects.txt', 'r+') as list_projects_file :  #la syntaxe 'with ... as :' permet d'éviter d'oublier un file.close, le fichier est automatique fermé en sortie de boucle    
     if len(list_projects_file.read()) == 0 :
         data = [[]]
         list_urls = []
         next_id = 0
     else :	
+    	list_projects_file.seek(0) # c'est là le trick: on a déjà lu le fichier l29, il faut rembobiner le fichier pour pouvoir le relire sinon il lira ce qu'il y a la suite de la fin, cad rien!
         data      =  [ project.strip().split(';') for project in list_projects_file.readlines() ]
         list_urls =   [ project[1] for project  in data ]   # data[:,1] ne marche pas c'est génial
         list_id   =  [float(project[0]) for project  in data ]
         next_id = max(list_id ) + 1
-        
+
     for new_project_url in projects_list() :
         if new_project_url not in list_urls :
             list_projects_file.write( str(next_id) + ';' + new_project_url +  '\n') 
