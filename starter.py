8ea8f20b8a01664440c60416077b6e2d44a18e4d
Subacultchah <subacultchah@googlemail.com>
2014-02-17 18:34:42 +0100

    correction all_project: on met un lock

--- starter.py
+++ starter.py
@@ -3,7 +3,8 @@
 
 import project_scrapping as ps
 import urllib
-from threading import Thread
+from threading import Thread, Lock
+
 
 with open('list_projects.txt', 'r') as list_projects_file : 
     list_projects= [project.strip().split(';') for project in list_projects_file ]
@@ -16,15 +17,17 @@ def parallel_run(fn, l):
 
 
 def projects_scanner(l):
-    #all_projects = {}
+    lock = Lock()
+    all_projects = {}
     def update(project):
         id , short_url = project
         long_url = 'http://www.kickstarter.com/projects/' + short_url
         project_page_contents = urllib.urlopen(long_url).read()
         data_project = ps.get_data(project_page_contents , ps.current_vars)
         id_date = id + '_' + data_project['SCRAPPING DAY']
-        #all_projects[id_date] = data_project
-        print data_project
+        with lock:
+            all_projects[id_date] = data_project
+            print data_project
     parallel_run(update,l)
 
 
