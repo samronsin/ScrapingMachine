9bb3744168acc9915e4672d718e348ac2f988aeb
sami3000 <sam.ronsin@gmail.com>
2014-02-17 01:56:04 +0100

    parallel http requests implemented (projects data update)

--- starter.py
+++ starter.py
@@ -1,26 +1,32 @@
 # -*- coding: utf-8 -*-
 # <nbformat>3.0</nbformat>
 
-import project_scrapping as pj
+import project_scrapping as ps
 import urllib
-
-
+from threading import Thread
 
 with open('list_projects.txt', 'r') as list_projects_file : 
     list_projects= [project.strip().split(';') for project in list_projects_file ]
 
-def projects_scanner(list_projets):
-    all_project = {}
-    for project in list_projets :
+#print list_projects
+
+def parallel_run(fn, l):
+       for i in l:
+           Thread(target = fn, args = (i,)).start()
+
+
+def projects_scanner(l):
+    #all_projects = {}
+    def update(project):
         id , short_url = project
-        long_url = 'http://www.kickstarter.com/projects/'+short_url
+        long_url = 'http://www.kickstarter.com/projects/' + short_url
         project_page_contents = urllib.urlopen(long_url).read()
-        data_project = pj.get_data(project_page_contents , pj.current_vars)
+        data_project = ps.get_data(project_page_contents , ps.current_vars)
         id_date = id + '_' + data_project['SCRAPPING DAY']
-        all_project[id_date] = data_project
-    return all_project
+        #all_projects[id_date] = data_project
+        print data_project
+    parallel_run(update,l)
 
 
 if __name__ == '__main__' :
-	for key, data in projects_scanner(list_projects).items():
-		print 'id is %s and data is %s' %(key, data)
+	projects_scanner(list_projects)
