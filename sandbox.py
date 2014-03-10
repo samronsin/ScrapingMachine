7ff6eb1dc42af7093649cbd7518f349546497170
sami3000 <sam.ronsin@gmail.com>
2014-03-10 17:59:43 +0100

    sandbox updated (not cleaned)

--- sandbox.py
+++ sandbox.py
@@ -21,6 +21,12 @@ def load_data(dir_name):
     return projects, statuses#, tweets, graph
 
 
+def load_tweets_data(dir_name):
+    print 'Loading tweets...'
+    tweets = np.load( dir_name + '/tweets.npy')
+    return tweets
+
+
 def print_project(num_project):
     project = projects[num_project]
     project_statuses = statuses[num_project]
@@ -41,6 +47,18 @@ def print_status(num_project,num_status):
     print 'True time: ' + str(status[0] * (project[4] - project[3]) + project[3])
     print 'Pledged money: ' + str(status[1])
     print 'Number of backers: ' + str(status[2])
+
+
+def print_tweets(num_project,num_tweet):
+    tweet = tweets[num_project,num_tweet]
+    project = projects[num_project]
+    print str(num_tweet) + 'th sample of tweets of the ' + str(num_project) + 'th project:'
+    print 'Time: ' + str(tweets[num_project, num_tweet, 0])
+    print 'Number of tweets: ' + str(tweets[num_project, num_tweet, 1])
+    print 'Number of replies: ' + str(tweets[num_project, num_tweet, 2])
+    print 'Number of retweets: ' + str(tweets[num_project, num_tweet, 3])
+    print 'Estimated number of backers: ' + str(tweets[num_project, num_tweet, 4])
+    print 'Number of users who tweeted: ' + str(tweets[num_project, num_tweet, 5])
 
 
 def accuracy(predictions,Y):
@@ -149,13 +167,58 @@ for date in dates:
     X_5 = statuses[:,date/2,1]   
     XX = np.vstack((X,X_1,X_2,X_3,X_4,X_5)).T
     accuracies_l_.append(accuracy(lag_logit_predict(XX,Y),Y))
-### Observations: 
+### Observations: lags don't provide significant explanatory power
+
+
+### Testing for fitting logistic with additionnal dummies
+def dummies_logit_predict(XX,Y):
+    lr = linear_model.LogisticRegression()
+    lr.fit(XX,Y)
+    return lr.predict(XX)
+
+
+money_lost = np.zeros_like(statuses[:,0,1])
+is_success = np.zeros_like(statuses[:,0,1])
+
+
+accuracies_l_ = []
+for date in dates:
+    X = statuses[:,date,1]
+    date_lag = date - 10 if date >=10 else 0
+    X_lag = statuses[:,date_lag,1]
+    for j in range(len(money_lost)):
+        money_lost[j] = money_lost[j] or (X[j] < X_lag[j])
+        is_success[i] = float( X[j] >= 1.0 )
+    XX = np.vstack((X,money_lost#,
+        #is_success
+        )).T
+    accuracies_l_.append(accuracy(dummies_logit_predict(XX,Y),Y))
+### Observation:   dummy on losing backers' money provides siginificant explanatory power
+### Observation:   dummy on being already successful don't provide any explanatory power
+
+
+### Testing for fitting logistic with tweets data
+
+
+accuracies_l_t = []
+for t in dates:
+    X0 = statuses[:,t,1]
+    Tw1 = tweets[:,t,1]#Tweets
+    Tw2 = tweets[:,t,2]#Retweets
+    Tw3 = tweets[:,t,3]#Replies
+    Tw4 = tweets[:,t,4]#Estimation of number of backers
+    Tw5 = tweets[:,t,5]#Users who tweeted
+    XX = np.vstack((X0,
+    Tw1,Tw2,Tw3,Tw4,Tw5
+        )).T
+    accuracies_l_t.append(accuracy(lag_logit_predict(XX,Y),Y))
+### Observation:   dummy on losing backers' money provides siginificant explanatory power
 
 
 pylab.plot(dates,accuracies, label = 'benchmark')
 pylab.plot(dates,accuracies_l, label = 'simple logistic')
 #pylab.plot(dates,accuracies_l_, label = 'logistic with zooms')
-pylab.plot(dates,accuracies_l_, label = 'logistic with lag')
+pylab.plot(dates,accuracies_l_t, label = 'logistic with tweets data')
 #pylab.plot(dates,accuracies_l__)
 pylab.title('Accuracy of logistic predictors')
 pylab.xlabel('time')
@@ -164,7 +227,8 @@ pylab.grid(True)
 pylab.xticks(np.arange(0,1000,100))
 pylab.yticks(np.arange(0.5,1.05,0.05))
 pylab.legend(loc='lower center',fancybox=True)
-pylab.savefig('logistic-2.png')
+pylab.savefig('logistic-4.png')
+pylab.size(12)
 pylab.show()
 
 
@@ -201,6 +265,7 @@ pylab.plot(dates,alpha_1, label = "hyperbolic 0.01")
 pylab.plot(dates,alpha_2, label = "hyperbolic 1 (cap 100)")
 pylab.legend()
 pylab.show()
+
 ### Observations:   - pretty stable
 #                   - magnitude 
 
@@ -277,9 +342,10 @@ dates_loss = np.zeros_like(statuses[:,0,1])
 for i in range(len(probas)-1):
     Xt = statuses[:,dates[i],1]
     Xtplus1 = statuses[:,dates[i+1],1]
-    for j in range(len(money_lost)):
-        money_lost[j] = money_lost[j] or (Xtplus1[j] < Xt[j])
+    
         if dates_loss[j] == 0. and  Xtplus1[j] < Xt[j]: dates_loss[j] = dates[i]
 
-    
+
+###Taking tweets into account
+
 
